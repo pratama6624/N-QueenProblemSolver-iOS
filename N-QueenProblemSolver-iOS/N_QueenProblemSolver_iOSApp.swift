bf91e6d5098c999bf34788b43b87744dcc5efac2
pratama6624 <scyoung6624@gmail.com>
2024-10-22 22:34:47 +0700

    Initial Commit

---
+++ N-QueenProblemSolver-iOS/N_QueenProblemSolver_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  N_QueenProblemSolver_iOSApp.swift
+//  N-QueenProblemSolver-iOS
+//
+//  Created by Pratama One on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct N_QueenProblemSolver_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
